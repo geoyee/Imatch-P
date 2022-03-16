3804d1316f587a89a3c89dc8cf1cc8adbed6449c
Yizhou Chen <geoyee@yeah.net>
2022-03-16 10:27:43 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,13 +1,19 @@
 from models import Matching, read_image, make_matching_plot_fast
 
 
-if __name__ == "__main__":
+def matching_and_display(im0_path, im1_path):
     model = Matching()
-    im0, imt0 = read_image("assets/A.jpg")
-    im1, imt1 = read_image("assets/B.jpg")
+    im0, imt0 = read_image(im0_path)
+    im1, imt1 = read_image(im1_path)
     data = {"image0": imt0, "image1": imt1}
     pred = model(data)
     # print(pred)
-    make_matching_plot_fast(
+    out = make_matching_plot_fast(
         im0, im1, pred["keypoints0"], pred["keypoints1"], 
         pred["matches0"], pred["matches1"])
+
+
+if __name__ == "__main__":
+    im0_path = "assets/A.jpg"
+    im1_path = "assets/B.jpg"
+    matching_and_display(im0_path, im1_path)
